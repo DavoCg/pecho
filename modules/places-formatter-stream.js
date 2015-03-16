8d571440965418fc6bac0c3a92cb1a06712e63eb
David Cingala <david.cingala@gmail.com>
2015-03-16 23:14:29 +0100

    add percent of matching

--- modules/places-formatter-stream.js
+++ modules/places-formatter-stream.js
@@ -27,19 +27,32 @@ PlaceFormatterStream.prototype._transform = function(query, encoding, done){
     });
 
     function augment(place, callback){
-        
+
         var lat = place.location[1];
         var lon = place.location[0];
 
+        /**
+         * distance between a place and the user geoloc
+         */
         var distance = geolib.getDistance(
             {latitude: lat, longitude: lon},
             {latitude: queryLat, longitude: queryLon}
         );
-
         place.distance = distance;
+        /**
+         * percent of matching between query hashtags and place hashtag
+         * 100% if all query hashtags are present in place hashtags
+         */
+        place.matching = getMatching(query.hashtags, place.hashtags);
+        
         return callback(null, place);
     }
 };
+
+function getMatching(firstArray, secondArray){
+    return (_.intersection(firstArray, secondArray).length / firstArray.length) * 100
+
+}
 
 module.exports = function(){return new PlaceFormatterStream();};
 
