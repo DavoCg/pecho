3b41003a5b3ee48984388c74da93c27fa1c10f37
David Cingala <david.cingala@gmail.com>
2015-03-23 20:36:35 +0100

    Refactor (req,res) no more in controller, directly pass arguments in routes + update matching-calculator

--- app/routes/places.js
+++ app/routes/places.js
@@ -2,8 +2,19 @@ var places = require('controllers').places;
 var authenticator = require('authenticator');
 
 var placesRoutes = function(app){
-    app.get('/places', authenticator, places.getPlaces);
-    app.get('/places/:id', authenticator, places.getPlace);
+    app.get('/places', authenticator, function getPlaces(req, res, next){
+        places.getPlaces(req.query, function(err, places){
+            if(err) return next(err);
+            return res.status(200).send(places);
+        });
+    });
+    app.get('/places/:id', authenticator, function getPlace(req, res, next){
+        places.getPlace(req.query, function(err, place){
+            if(err) return next(err);
+            if(!place) return res.status(404).send('Not Found');
+            return res.status(200).send(place);
+        });
+    });
 };
 
 module.exports = placesRoutes;
