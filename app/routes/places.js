1de6c615268456a7b0848c75ea41588a179830a5
David Cingala <david.cingala@gmail.com>
2015-04-16 21:47:37 +0200

    remove authenticator middleware for test purpose

--- app/routes/places.js
+++ app/routes/places.js
@@ -2,13 +2,13 @@ var places = require('controllers').places;
 var authenticator = require('authenticator');
 
 var placesRoutes = function(app){
-    app.get('/places', authenticator, function getPlaces(req, res, next){
+    app.get('/places', /*authenticator,*/ function getPlaces(req, res, next){
         places.getPlaces(req.query, function(err, places){
             if(err) return next(err);
             return res.status(200).send(places);
         });
     });
-    app.get('/places/:id', authenticator, function getPlace(req, res, next){
+    app.get('/places/:id', /*authenticator,*/ function getPlace(req, res, next){
         var params = {query: req.query, id: req.params.id};
         places.getPlace(params, function(err, place){
             if(err) return next(err);
@@ -16,7 +16,7 @@ var placesRoutes = function(app){
             return res.status(200).send(place);
         });
     });
-    app.post('/places', authenticator, function addPlace(req, res, next){
+    app.post('/places', /*authenticator,*/ function addPlace(req, res, next){
         var place = req.body;
         place.location[0] = +place.location[0];
         place.location[1] = +place.location[1];
