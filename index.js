578ee471b32d8360f39cb04dc41a5562b4e15867
David Cingala <david.cingala@gmail.com>
2015-05-22 22:07:27 +0200

    modify index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var app = require('./app');
 
 app.listen(app.config.port, app.config.bind, function(){
-    console.log('Server listen port' + app.config.port);
+    console.log('Server listen port ' + app.config.port);
 });
