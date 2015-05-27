ec6fedfaa7bfb2cc71d5aa3664b86794d79d1293
David Cingala <david.cingala@gmail.com>
2015-05-27 23:40:10 +0200

    modify es-init to add a place owner + add authentication for owner to update his place + add route editPlace + remove all routes about user

--- utils/init-es.js
+++ utils/init-es.js
@@ -1,24 +1,53 @@
 var request = require('superagent');
 var async = require('async');
-var template = require('./fixtures/template.json');
+var filler = require('./filler');
+var templatePlaces = require('./fixtures/template-places.json');
+var templateOwners = require('./fixtures/template-owners.json');
 
-async.series([deletePlaces, addTemplate], function(err, results){
+async.series([deletePlaces, deleteOwners, addPlacesTemplate, addOwnersTemplate, fill], function(err, results){
     if(err) return console.log(err);
     return console.log('FINISH');
 });
 
 function deletePlaces(callback){
     request
-        .del('http://localhost:9200/places')
-        .end(callback);
+        .head('http://localhost:9200/places')
+        .end(function(err, res){
+            if(res.statusCode === 404) return callback();
+            request
+                .del('http://localhost:9200/places')
+                .end(callback);
+        })
 }
 
-function addTemplate(callback){
+function deleteOwners(callback){
+    request
+        .head('http://localhost:9200/owners')
+        .end(function(err, res){
+            if(res.statusCode === 404) return callback();
+            request
+                .del('http://localhost:9200/owners')
+                .end(callback);
+        })
+}
+
+function addPlacesTemplate(callback){
     request
         .put('http://localhost:9200/_template/places')
         .type('json')
-        .send(template)
+        .send(templatePlaces)
         .end(callback);
 }
 
+function addOwnersTemplate(callback){
+    request
+        .put('http://localhost:9200/_template/owners')
+        .type('json')
+        .send(templateOwners)
+        .end(callback);
+}
+
+function fill(){
+    return filler();
+}
 
